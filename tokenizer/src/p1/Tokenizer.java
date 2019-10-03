32beefbcd8cdfb48eefdb8a372f8023e1caf13ad
BriceBrecheisen <brecheisen.brice@gmail.com>
2019-10-03 18:51:53 -0500

    pdfs

--- tokenizer/src/p1/Tokenizer.java
+++ tokenizer/src/p1/Tokenizer.java
@@ -1,5 +1,28 @@
+
 package p1;
 
-public class Tokenizer {
+import java.util.ArrayList;
 
+
+public class Tokenizer 
+{
+	//handles words
+    private String word;
+    //array handles reserved words
+    private String[] res_words = new String[]{"class","constructor","method","function","int","boolean",
+    		"char","void","var","static","field","let","do","if","else","while","return","true","false",
+    		"null","this"};
+    
+    private String s1 = "([])({}|.,;+-*/&<>="; //symbols stored as string  
+    private char[] symbols = s1.toCharArray(); //symbols parsed out into a char array 
+    private String[] comment = new String [] {"//","*/","*/","/**"}; //handles comments
+    
+    private ArrayList<String> a1 = new ArrayList<String>();
+    private ArrayList<String> a2 = new ArrayList<String>();
+    
+    //default constructor
+    public Tokenizer() {}
 }
+    
+    
+
